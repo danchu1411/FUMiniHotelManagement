8e65c9278ad5d366443f1597433b3a38a442ba90
danchu1411 <dangctde180178@fpt.edu.vn>
2026-06-24 20:57:20 +0700

    Implement singleton RoomTypeDAO with CRUD operations
    Refactored RoomTypeDAO to be public and use the singleton pattern. Added CRUD methods for RoomType entities using FuminiHotelManagementContext. Removed unused using directives.

--- DataAccessLayer/RoomTypeDAO.cs
+++ DataAccessLayer/RoomTypeDAO.cs
@@ -1,12 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BusinessObjects;
 
 namespace DataAccessLayer
 {
-    internal class RoomTypeDAO
+    public class RoomTypeDAO
     {
+        private static RoomTypeDAO? instance = null;
+        private static readonly object instanceLock = new object();
+
+        private RoomTypeDAO() { }
+
+        public static RoomTypeDAO Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new RoomTypeDAO();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public List<RoomType> GetAllRoomTypes()
+        {
+            using var context = new FuminiHotelManagementContext();
+            return context.RoomTypes.ToList();
+        }
+
+        public RoomType? GetRoomTypeById(int roomTypeId)
+        {
+            using var context = new FuminiHotelManagementContext();
+            return context.RoomTypes.FirstOrDefault(rt => rt.RoomTypeId == roomTypeId);
+        }
+
+        public void AddRoomType(RoomType roomType)
+        {
+            using var context = new FuminiHotelManagementContext();
+            context.RoomTypes.Add(roomType);
+            context.SaveChanges();
+        }
+
+        public void UpdateRoomType(RoomType roomType)
+        {
+            using var context = new FuminiHotelManagementContext();
+            context.RoomTypes.Update(roomType);
+            context.SaveChanges();
+        }
+
+        public void DeleteRoomType(RoomType roomType)
+        {
+            using var context = new FuminiHotelManagementContext();
+            context.RoomTypes.Remove(roomType);
+            context.SaveChanges();
+        }
     }
 }
